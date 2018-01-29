f1a9bc1bdd4ad412ef1c796d959d7fc65368a447
Zhu Fengyuan <zhufyaxel@gmail.com>
2018-01-28 19:02:56 -0500

    Standard_Reflection&Refraction

---
+++ Assets/Scripts/RenderCubeMapRealTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RenderCubeMapRealTime : MonoBehaviour
+{
+
+    public Cubemap cubemap;
+    public GameObject go;
+    public void Update()
+    {
+        // create temporary camera for rendering
+       // place it on the object
+        go.GetComponent<Camera>().RenderToCubemap(cubemap);
+        
+    }
+}
